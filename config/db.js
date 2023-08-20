10783b3b2bb6b0569cd39b682e09b370bcfc7c8b
lade6501 <lade6501@gmail.com>
2023-08-20 23:46:21 +0530

    Exporting mongodb client

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+
 const db_url = process.env.DB_URL;
+//Creating a mongodb client
+const mongoClient = new MongoClient(db_url);
 
 module.exports = {
+  mongoClient,
   url: db_url,
   database: "files_db",
   bucketName: "MyFiles",
